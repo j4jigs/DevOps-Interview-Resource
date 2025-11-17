ad4fbf5b4bb04c1934c8c81e65b2a2343c1d2a68
Jignesh Patel <talktojignesh@gmail.com>
2025-11-17 16:28:46 +0530

    updated workflow

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP CI/CD Test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+
+    <h1>Welcome to PHP CI/CD Test Page Dipesh and Jignesh</h1>
+    <p>This page is deployed using GitHub Actions → AWS EC2.</p>
+
+    <a href="about.php" class="btn">About Page</a>
+    <a href="test.php" class="btn">Test PHP Page</a>
+
+</div>
+</body>
+</html>
